48ff9728b0f4ad1292d183a81fe873544c835cbb
PnoJai <pnojai@protonmail.com>
2019-01-31 11:22:20 -0500

    Added notes for mySQL and HDF5.

--- 03_GetClean/week1_notes.R
+++ 03_GetClean/week1_notes.R
@@ -37,7 +37,7 @@ cameraData <- loadWorkbook(paste(data_path, "TEST.xlsx", sep = "/"), create = TR
 # snapshot a VM that I can roll back to and eliminate Java.
 
 # That piece of crap run-time is gone from my VM. Let's try a package that doesn't depend on Java.
-# Installed openxml from the developer page. The stable build on CRAN doesn't work on R v3.5.2.
+# Installed openxlsx from the developer page. The stable build on CRAN doesn't work on R v3.5.2.
 # Developer website: https://github.com/awalker89/openxlsx
 # Installed by loading the devtools library and running: install_github("awalker89/openxlsx")
 
@@ -53,7 +53,21 @@ fileURL <- "https://www.w3schools.com/xml/simple.xml"
 file_name <- "simple.xml"
 doc <- xmlTreeParse(fileURL, useInternalNodes = TRUE) # Doesn't work. Must be junk at the top of the file.
 
-# Worked when I downloaded it and opened it.
+# Junk, yes and no. xmlTreeParse doesn't support the HTTPS protocol, which encrypts the transmission.
+# Chiranjib Sardor, on Coursera, found a solution. Use RCurl, which does support HTTPS.
+library(RCurl)
+xmlURL.1 <- "https://www.w3schools.com/xml/simple.xml"
+xmlURL.2 <- getURL(xmlURL.1)
+
+head(xmlURL.2) # That has data in it.
+
+# Use the 2nd step URL. Now it works.
+doc <- xmlTreeParse(xmlURL.2, useInternalNodes = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+
+# Before learning about RCurl, I worked around the HTTPS problem by downloading and opening it.
 download.file(fileURL, destfile = paste(data_path, file_name, sep = "/"))
 doc <- xmlTreeParse(paste(data_path, file_name, sep = "/"), useInternalNodes = TRUE)
 rootNode <- xmlRoot(doc)
@@ -130,10 +144,120 @@ file_name <- "acs_micro_ID2006.csv"
 acs_data <- read.csv(paste(data_path, file_name, sep = "/"))
 head(acs_data)
 
-head(acs_data$VAL)
-
 # 1. How many properties are worth $1,000,000 or more
 # Variable VAL records property value.
 # Code 24 indicates $1,000,000 or more
 
+head(acs_data$VAL)
 
+# Your SQL head thinks about SELECT first and filter (WHERE) second.
+# In data frame notation, the first index expresses the filter condition,
+# the second index expresses the select.
+acs_data[acs_data$VAL == 24 & !is.na(acs_data$VAL), "VAL"]
+
+# This result is a vector now, not a data frame.
+str(acs_data[acs_data$VAL == 24 & !is.na(acs_data$VAL), "VAL"])
+
+# You see them. Now count them. A vector, so length(), not nrow() like a data frame.
+length(acs_data[acs_data$VAL == 24 & !is.na(acs_data$VAL), "VAL"])
+
+# 3 Download Excel spreadsheet.
+library(openxlsx)
+
+file_name <- "NaturalGasAcquisitionProgram.xlsx"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+
+if (!file.exists(paste(data_path, file_name, sep = "/"))) {
+        download.file(fileURL, destfile = paste(data_path, file_name, sep = "/"))
+} else {
+        print( paste("File exists, download skipped:", file_name))
+}
+
+dateDownloaded <- date()
+dateDownloaded # "Mon Jan 21 11:58:05 2019"
+
+dat <- read.xlsx(paste(data_path, file_name, sep = "/"),
+                 rows = 18:23, cols = 7:15)
+str(dat)
+
+sum(dat$Zip*dat$Ext,na.rm=T)
+
+# 4 Download XML file.
+library(XML)
+
+file_name <- "BaltimoreRestaurants.xml"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+
+if (!file.exists(paste(data_path, file_name, sep = "/"))) {
+        download.file(fileURL, destfile = paste(data_path, file_name, sep = "/"))
+} else {
+        print( paste("File exists, download skipped:", file_name))
+}
+
+dateDownloaded <- date()
+dateDownloaded # "Mon Jan 21 12:10:14 2019"
+
+doc <- xmlTreeParse(paste(data_path, file_name, sep = "/"), useInternalNodes = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+
+# How many zipcode values = 21231.
+# I solved it by picking off all zipcode values and then summing the matches.
+# I'm sure you could filter this in XPath, too.
+zipcodes <- xpathSApply(doc, "//zipcode", xmlValue)
+sum(zipcodes == "21231")
+
+#5. Performance.
+# The download file is not the exact same file as question #1.
+library(data.table)
+
+file_name <- "acs_micro_ID2006_2.csv"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+
+if (!file.exists(paste(data_path, file_name, sep = "/"))) {
+        download.file(fileURL, destfile = paste(data_path, file_name, sep = "/"))
+} else {
+        print( paste("File exists, download skipped:", file_name))
+}
+
+dateDownloaded <- date()
+dateDownloaded # "Mon Jan 21 12:26:11 2019"
+
+DT <- fread(file = paste(data_path, file_name, sep = "/"))
+
+head(DT)
+str(DT)
+
+head(DT$pwgtp15)
+
+# Hmm. I wonder if there is a way to lapply commands.
+# cmds <- list("DT[,mean(pwgtp15),by=SEX]")
+# lapply(cmds, system.time)
+
+DT[,mean(pwgtp15),by=SEX]
+sapply(split(DT$pwgtp15,DT$SEX),mean)
+# No good. mean(DT$pwgtp15,by=DT$SEX)
+mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)
+# No good. rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]
+tapply(DT$pwgtp15,DT$SEX,mean)
+
+print("BEGIN PERFORMANCE TEST")
+system.time(DT[,mean(pwgtp15),by=SEX])
+
+system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
+
+# Apparent winner. But incorrect result.
+# system.time(mean(DT$pwgtp15,by=DT$SEX))
+
+system.time({mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)})
+
+# system.time({rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]})
+
+system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+
+# Try saving the results
+test_timings <- system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+str(test_timings)
+
+# I'm unsure how useful that is for this assignment. I'm moving on for now.
---
+++ 03_GetClean/week2_notes.R
@@ -0,0 +1,116 @@
+# 1. mySQL
+# Build a mySQL server.
+# Nice to have. Save that for its own VM when I get around to it.
+
+# Install package RMySQL
+# Ubuntu requires deb: libmariadbclient-dev | libmariadb-client-lgpl-dev (Debian, Ubuntu)
+# This appears to require installation of mySQL. You can't just install the R package
+# and connect to an existing server.
+
+# Sample code. Connections, querying, etc.
+# Do this later when you build an environment to support it.
+
+# Connect to a server and list databases. Remember to disconnect.
+ucscDb <- dbConnect(MySQL(),user="genome",
+                   host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb,"show databases;"); dbDisconnect(ucscDb);
+
+# Connect to a databas and list databases.
+hg19 <- dbConnect(MySQL(),user="genome",db="hg19",
+                  host="genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+
+allTables(1:5)
+
+# Get dimensions of a particular table.
+dbListFields(hg19, "affyU133Plus2")
+
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+
+# Read from the table
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+head(affyData)
+
+# Select a subset
+# Stores the result on the server...
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+# Retrieves the results...
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+
+# You can look at a small subset of the result stored on the server.
+# Here, the top 10. Then clear the result on the server.
+affyMisSmall <- fetch(query,n=10); dbClearResult(query);
+
+dim(affyMisSmall)
+
+# Close the connection
+dbDisconnect(hg19)
+
+# 2. HDF5
+# Hierarchical Data Format
+# Large data sets
+# Variety of data types
+#
+# Groups containing zero or more data sets and metadata.
+#   Group header with group name and list of attributes.
+#   Group symbol table with list of objects in group.
+#
+# Data sets, multidimensional array of data elements with metadata.
+#   Header with name, datatype, dataspace, and storage layout.
+#   Data array with the data.
+#
+# http://www.hdfgroup.org
+
+# Get the R HDF5 package.
+# Already installed bioconductor installer, no? Guess not.
+# Lecture modeled after:
+# http://bioconductor.org/packages/release/bioc/vignettes/rhdf5/inst/doc/rhdf5.pdf
+# Try this link:
+# http://bioconductor.org/packages/devel/bioc/vignettes/rhdf5/inst/doc/practical_tips.html
+# Really the vignette with the package. I was able to install the package, so I could look there.
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+
+library(rhdf5)
+file.remove("example.h5")
+created <- h5createFile("example.h5")
+created # Should equal TRUE
+
+created <- h5createGroup("example.h5", "foo")
+created <- h5createGroup("example.h5", "baa")
+created <- h5createGroup("example.h5", "foo/foobaa")
+h5ls("example.h5")
+
+# Write to groups.
+A <- matrix(1:10, nr = 5, nc = 2)
+h5write(A, "example.h5", "foo/A")
+B <- array(seq(0.1, 2.0, by = 0.1, ), dim = c(5, 2, 2))
+attr(B, "scale") <- "liter"
+h5write(B, "example.h5", "foo/foobaa/B")
+
+h5ls("example.h5")
+
+# Write a dataset.
+df <- data.frame(1L:5L, seq(0, 1, length.out = 5),
+                 c("ab", "cde", "fghi", "a", "s"), stringsAsFactors = FALSE)
+
+h5write(df, "example.h5", "df")
+
+h5ls("example.h5")
+
+# Read.
+readA <- h5read("example.h5", "foo/A")
+readB <- h5read("example.h5", "foo/foobaa/B")
+readdf <- h5read("example.h5", "df")
+
+readA
+readB
+readdf
+
+# Writing and reading in chunks.
+h5write(c(12, 13, 14), "example.h5", "foo/A", index = list(1:3, 1))
+# This is a READ, not an LS.
+# The index for writing can also be used for reading.
+h5read("example.h5", "foo/A")
