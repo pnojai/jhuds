8b8d97d7d4f8cea23abcab5e239f598a3b342549
PnoJai <pnojai@protonmail.com>
2019-02-02 11:40:07 -0500

    Wrapped up week 3.

--- 03_GetClean/week2_notes.R
+++ 03_GetClean/week2_notes.R
@@ -176,3 +176,144 @@ pg1
 # APIs
 # Usually use GET requests with specific URLs as arguments.
 # Usually need a developer account for the API.
+
+# None of this worked as presented. If I need it, I'll drill into it later.
+# Twitter. PnoJai app.
+# Consumer API keys
+# BvGNFQ077eIUu2jczx7CfQsiA (API key)
+# EneR5yjka25OFQgn9e7F8vk7aQukxkC5GNdsgEFZAVW89lKiPw (API secret key)
+# 
+# Access token & access token secret
+# 294633077-ENungp0S0nMw05m74jyqhZDW6ZZEemDBKN4baByj (Access token)
+# H0P970NjETukx9vQ5atTmdaliZ0NhpqGozSf6KrQ6E847 (Access token secret)
+# Read and write (Access level)
+library(httr)
+myapp <- oauth_app("twitter",
+                  key = "BvGNFQ077eIUu2jczx7CfQsiA", #Consumer Secret Key"
+                  secret = "EneR5yjka25OFQgn9e7F8vk7aQukxkC5GNdsgEFZAVW89lKiPw") # Your consumer secret key
+sig <- sign_oauth1.0(myapp,
+                     token = "294633077-ENungp0S0nMw05m74jyqhZDW6ZZEemDBKN4baByj", # Your token
+                     token_secret = "EneR5yjka25OFQgn9e7F8vk7aQukxkC5GNdsgEFZAVW89lKiPw")
+2
+homeTL <- GET("https://api.twitter.com/1.1/statuses/home_timeline.json", sig)
+homeTL
+
+json1 <- content(homeTL)
+json1
+
+json2 <- jsonlite::fromJSON(toJSON(json1))
+json2[1, 1:4]
+
+# Other data.
+# Music
+# tuneR on CRAN
+# seewave. http://rug.mnhn.fr/seewave/
+
+# Quiz.
+# More instruction at quiz, accessing API at GitHub.
+# A page of API demos using httr.
+# https://github.com/r-lib/httr/tree/master/demo
+# Using now examples from the GitHub demo.
+
+library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. To make your own application, register at
+#    https://github.com/settings/developers. Use any URL for the homepage URL
+#    (http://github.com is fine) and  http://localhost:1410 as the callback url
+#
+#    Replace your key and secret below.
+myapp <- oauth_app("github",
+                   key = "82f6ffe8eddf8f9c0870",
+                   secret = "9a11c4108199a156045c42fbad58f18c64c02015"
+)
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+1 # Unlike my Twitter attempt, this time I selected saving a local file.
+  # httr opened my browser and I manually authorized my app.
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/rate_limit", gtoken)
+stop_for_status(req)
+content(req)
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
+stop_for_status(req)
+content(req)
+
+# Quiz.
+
+# Question 1.
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+ct <- content(req)
+
+out <- capture.output(str(ct))
+
+cat(out, file = "03_GetClean/Data/ct_str.txt", sep = "\n")
+
+head(ct)
+
+ct[[16]]
+
+# It was a lame fishing expedition, but I found the answer by browsing.
+
+# 2.
+# Get the package sqldf.
+# Hey, that thing lets me write SQL.
+library(sqldf)
+
+file_name <- "03_GetClean/Data/acs_micro_ID2006.csv"
+acs <- read.csv(file_name)
+
+sqldf("select * from acs")
+
+# unique(acs$...
+
+# 4. Count characters in HTML lines.
+# http://biostat.jhsph.edu/~jleek/contact.html
+
+con <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode <- readLines(con)
+close(con)
+htmlCode
+
+lins <- htmlCode[c(10, 20, 30, 100)]
+
+# I finally understood and was able to use lapply().
+lapply(lins, nchar)
+
+# 5. Get this file and report the sum of the 4th of 9 columns
+# https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for
+
+file_name <- "03_GetClean/Data/wksst8110.for"
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+
+if (!file.exists(file_name)) {
+        download.file(fileURL, destfile = file_name)
+} else {
+        print( paste("File exists, download skipped:", file_name))
+}
+
+dateDownloaded <- date()
+dateDownloaded # "Sat Feb  2 10:55:16 2019"
+
+# Fixed width.
+# What the hell, just try CSV and see what happens.
+dat <- read.csv(file_name, header = TRUE, skip = 3)
+str(dat)
+
+# That's what I thought. Each line is an entire string. And it's a factor!
+# The header names are not unique. And if you do include them, the text makes the
+# whole column read as a character vector.
+dat <- read.fwf(file = file_name, widths = c(15, 4, 9, 4, 9, 4, 9, 4, 4), header = FALSE, skip = 4,
+                stringsAsFactors = FALSE)
+str(dat)
+
+sum(dat$V4)
