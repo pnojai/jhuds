cecc296a7b91cdb0f6b449e925ad8a225307709b
PnoJai <pnojai@protonmail.com>
2019-03-09 12:00:43 -0500

    Added notes about Lattice.

--- 04_EDA/week2_notes.R
+++ 04_EDA/week2_notes.R
@@ -34,3 +34,39 @@ p <- xyplot(Ozone ~ Wind, data = airquality)
 print(p)
 
 # Lattice panel functions.
+# Each panel gets a subset defined by the conditioning variable.
+
+# Generate some random data to look at this.
+set.seed(10)
+x <- rnorm(100)
+f <- rep(0:1, each = 50)
+y <- x + f - f * x + rnorm(100, sd = 0.5)
+f <- factor(f, labels = c("Group 1", "Group 2"))
+xyplot(y ~ x | f, layout = c(2, 1))
+
+# Custom panel function.
+xyplot(y ~ x | f, panel = function(x, y, ...) {
+        panel.xyplot(x, y, ...) # First, call the default panel function for xyplot.
+        panel.abline(h = median(y), lty = 2) # Add a horizontal line at the median.
+                
+})
+
+# Custom panel function. Add a regression line.
+xyplot(y ~ x | f, panel = function(x, y, ...) {
+        panel.xyplot(x, y, ...) # First, call the default panel function for xyplot.
+        panel.lmline(x, y, col = 2) # Add a regression line.
+})
+
+# ggplot2
+# Grammar of Graphics says that a statistical graphic is a mapping from data to aesthetic
+# attributes (color, shape, size) of geometric objects (points, lines, bars). The plot may also
+# contain statistical transformations of the data and is drawn on a specific coordinate system.
+
+# Basics: qplot. (Quick Plot). Analagous to base system plot().
+# Data: always in a data frame.
+# Plots made up of aesthetics and geoms.
+
+# Factors are important for indicating subsets. They should be labelled. For example, gender.
+
+# qplot hides details, which is often useful.
+# ggplot is the core function and very flexible for doing things qplot cannot.
